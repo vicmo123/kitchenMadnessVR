2a2b30a1301ad3f6a7563151ef4ff8b43c3a40e2
rakno <61773264+rakno@users.noreply.github.com>
2023-02-08 14:25:31 -0500

    knife primary changes in prefab

--- Assets/Scripts/Knife/Knife.cs
+++ Assets/Scripts/Knife/Knife.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Knife : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Transform CurrentKnifePosition;
+
     void Start()
     {
         
