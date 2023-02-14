ad95dc9206c297253aefde307164a297adba009a
TommyPoulin <2120044@isi-montreal.com>
2023-02-14 14:54:46 -0500

    Burnable component
    Added a fade to the grilled texture
    Added a delegate to set the burnable ingredient ready

--- Assets/Scripts/Grill/Burnable.cs
+++ Assets/Scripts/Grill/Burnable.cs
@@ -10,9 +10,21 @@ public class Burnable : MonoBehaviour
         Burning
     }
 
-    public float burningTime = 0;
-    public float grillingTime = 0;
+    public float burningTime = 3;
+    public float grillingTime = 10;
+
+    float initialGrillingTime;
+    float initialBurningTime;
+
     private State state;
+
+    public Material material1;
+    public Material material2;
+    public Renderer rend;
+
+    public delegate void StateDelegate();
+    [HideInInspector] public StateDelegate grilledDelegate;
+    [HideInInspector] public StateDelegate burntDelegate;
 
     private void Start() {
         if (grillingTime <= 0) {
@@ -20,6 +32,11 @@ public class Burnable : MonoBehaviour
         } else {
             state = State.Grilling;
         }
+
+        rend.material = material1;
+
+        initialGrillingTime = grillingTime;
+        initialBurningTime = burningTime;
     }
 
     public void OnGrill(Burner burner) {
@@ -38,9 +55,21 @@ public class Burnable : MonoBehaviour
 
     private void Grilled() {
         state = State.Burning;
+
+        grilledDelegate.Invoke();
     }
 
     private void Burnt() {
         Destroy(this);
+
+        burntDelegate.Invoke();
+    }
+
+    void Update() {
+        float lerp = (initialGrillingTime - grillingTime) / initialGrillingTime;
+        rend.material.Lerp(material1, material2, lerp);
+
+        //float burnLevel = 255 * burningTime / initialBurningTime;
+        //rend.material.color = new Color(burnLevel, burnLevel, burnLevel);
     }
 }
