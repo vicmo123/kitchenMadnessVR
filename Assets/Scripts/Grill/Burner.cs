a47a154282eb52db70b9dcfce59baa81d16cd0e6
TommyPoulin <2120044@isi-montreal.com>
2023-02-14 14:57:18 -0500

    Burner component
    Burner that calls OnGrill function of every Burnable components in contact
    Made it so that the more Burnable there is, the less it grills fast
    Added the grill a burner component

--- Assets/Scripts/Grill/Burner.cs
+++ Assets/Scripts/Grill/Burner.cs
@@ -5,4 +5,38 @@ using UnityEngine;
 public class Burner : MonoBehaviour
 {
     public float grillingMultiplier = 1;
+
+    float initialMultiplier;
+    int quantityGrilling;
+    float minimumMultiplier;
+    float nerfMultiplier;
+
+    private void Start() {
+        initialMultiplier = grillingMultiplier;
+        quantityGrilling = 0;
+        minimumMultiplier = .3f;
+        nerfMultiplier = .2f;
+    }
+
+    private void Update() {
+        grillingMultiplier = initialMultiplier - (quantityGrilling * nerfMultiplier);
+        if (grillingMultiplier < minimumMultiplier) {
+            grillingMultiplier = minimumMultiplier;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision) {
+        Burnable burnable = collision.gameObject.GetComponent<Burnable>();
+        if (burnable != null) {
+            burnable.OnGrill(this);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        quantityGrilling++;
+    }
+
+    private void OnCollisionExit(Collision collision) {
+        quantityGrilling--;
+    }
 }
