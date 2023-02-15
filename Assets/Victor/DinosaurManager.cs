ce2979f26f87c09dbf3f98feebf360a464d06b52
vicmo123 <vicmo26@gmail.com>
2023-02-15 15:47:37 -0500

    Dinosaurs in test scene
    - State machine for moving around
    - Dinosaur class for actions during the states
    - Dinosaur Manager for spawn logic

--- Assets/Victor/DinosaurManager.cs
+++ Assets/Victor/DinosaurManager.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class DinosaurManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject DinoPrefab;
+    public Transform ServiceZone;
+    private CountDownTimer timer;
+    private float duration = 10.0f;
+    private Vector2 timeRange = new Vector2(2.0f, 15.0f);
+    public float spawnRadius;
+    public bool roundActive { get; set; } = false;
+
+    private void Awake()
     {
-        
+        timer = new CountDownTimer(duration, true);
+        timer.StartTimer();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        timer.OnTimeIsUpLogic = () =>
+        {
+            duration = Random.Range(timeRange.x, timeRange.y);
+            timer.SetDuration(duration);
+        };
+    }
+
+    private void Update()
+    {
+        if (roundActive)
+        {
+            timer.UpdateTimer();
+        }
+    }
+
+    private void SpawnDinosaur()
+    {
+        Dinosaur dino = GameObject.Instantiate<GameObject>(DinoPrefab).GetComponent<Dinosaur>();
+        dino.EntryPoint = GetPosInsideUnitCircle();
+        dino.WaitForFoodPoint = ServiceZone.position;
+        dino.ExitPoint = dino.WaitForFoodPoint + GetPosInsideUnitCircle();
+    }
+
+    private Vector3 GetPosInsideUnitCircle()
+    {
+        float randomRadius = UnityEngine.Random.Range(spawnRadius, spawnRadius + 20.0f);
+        Vector2 insideCicle = (UnityEngine.Random.insideUnitCircle * randomRadius).normalized * randomRadius;
+        Vector3 spawnPoint = new Vector3(insideCicle.x, 0.5f, insideCicle.y);
+
+        return spawnPoint;
     }
 }
