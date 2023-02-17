60c8fcd122aa9167dddd2197a430aab341de0968
rakno <61773264+rakno@users.noreply.github.com>
2023-02-17 13:29:31 -0500

    Update ServingBell.cs

--- Assets/Scripts/Kitchen/ServingBell.cs
+++ Assets/Scripts/Kitchen/ServingBell.cs
@@ -4,44 +4,19 @@ using UnityEngine;
 
 public class ServingBell : MonoBehaviour
 {
-
-    public AudioClip bellSoundEffect;
-    private AudioSource bellAudioSource;
-
-
-    public AudioClip winSoundEffect;
-    private AudioSource winAudioSource;
-
-
-    public AudioClip loseSoundEffect;
-    private AudioSource loseAudioSource;
-
-    bool isServable;
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        bellAudioSource = GetComponent<AudioSource>();
-
-        winAudioSource = GetComponent<AudioSource>();
-
-        loseAudioSource = GetComponent<AudioSource>();
+  
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("VRHANDS"))
         {
-            bellAudioSource.PlayOneShot(bellSoundEffect);
-
-            if(isServable)
-            {
-                winAudioSource.PlayOneShot(winSoundEffect); //winSound;
-            }
-            else
-            {
-                loseAudioSource.PlayOneShot(loseSoundEffect); //loseSound;
-            }
+            SoundManager.BellDing.Invoke();
+           
         }
     }
 }
