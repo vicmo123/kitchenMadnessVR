24f98a5f8ad60b034b5942f0f918225f45e3ba91
Juan José  Cardona <68442034+Juanjocardona99@users.noreply.github.com>
2023-02-19 04:33:19 -0500

    Cuttable now cuts perfectly
    cuttable now can cut in all directions and  can cut until one piece is left

---
+++ Assets/Scripts/GenericComponents/Wedge.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wedge : MonoBehaviour
+{
+    public bool right;
+    public bool top;
+    public bool front;
+}
