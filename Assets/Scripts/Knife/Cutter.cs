f21ccfcf92124adbf18cc117c3faf2053dc1b395
Juan José  Cardona <68442034+Juanjocardona99@users.noreply.github.com>
2023-02-20 12:33:07 -0500

    Update Cutter.cs
    Cutter calls StopCut on Interface_Cutter when exiting the collider

--- Assets/Scripts/Knife/Cutter.cs
+++ Assets/Scripts/Knife/Cutter.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Cutter : MonoBehaviour
 {
-
+    InterFace_Cutter currentlyCutting;
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -18,16 +19,19 @@ public class Cutter : MonoBehaviour
         if (Physics.Raycast(transform.position, -(transform.forward), out hit, .2f, LayerMask.GetMask("Food")))
         {
             InterFace_Cutter ic = hit.collider.GetComponent<InterFace_Cutter>();
-            if(ic!=null)
+            if (ic != null)
             {
                 ic.Cut(hit);
-               
             }
-
-           
         }
+    }
 
-
-
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.Equals(currentlyCutting)) 
+        {
+            currentlyCutting.StopCut();
+            currentlyCutting = null;
+        }
     }
 }
