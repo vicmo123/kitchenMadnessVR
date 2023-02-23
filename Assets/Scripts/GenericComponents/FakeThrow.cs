75ea4f3579478f3c750827bb651745ce7f80f182
Léonie St-Onge <leonie.stonge@gmail.com>
2023-02-23 16:14:07 -0500

    Fake Thrown script
    Fake the throwing of an ingredient on the wall (with bigger collider now)

---
+++ Assets/Scripts/GenericComponents/FakeThrow.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeThrow : MonoBehaviour
+{
+    private Rigidbody rb;
+    public int force = 10;
+    private void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        Throw();
+    }
+
+    private void Throw()
+    {
+        rb.AddRelativeForce(new Vector3(-force, 0, 0), ForceMode.Impulse);
+    }
+}
