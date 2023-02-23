d4685d3d27d7150dff72a9c1e75f64beb6c0a166
DonJuanEsp <108744062+DonJuanEsp@users.noreply.github.com>
2023-02-23 13:24:20 -0500

    Started working on holding objects
    Holding objects properly
    
    Added a class to hold information for one of the options for the task

--- Assets/Scripts/Player/HandAnimation.cs
+++ Assets/Scripts/Player/HandAnimation.cs
@@ -8,12 +8,6 @@ public class HandAnimation : MonoBehaviour
     public InputActionProperty gripAnimationAction;
     public Animator handAnimator;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
